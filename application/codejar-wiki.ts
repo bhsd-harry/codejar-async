4dc543e3083de7060d834ec2e6fd7f8b003eb8bd
bhsd <2545473905@qq.com>
2024-07-11 11:12:05 +0800

    Revert "fix(codejar-wiki): styles"
    This reverts commit 73ad08bf45c08eec1f83dcfec21d417e0ae29d9b.

--- application/codejar-wiki.ts
+++ application/codejar-wiki.ts
@@ -6,11 +6,6 @@ declare const wikiparse: {
   print(text: string, include?: boolean, stage?: number, id?: number): Promise<[number, string, string][]>
 }
 
-const link = document.createElement('link')
-link.rel = 'stylesheet'
-link.href = 'https://testingcf.jsdelivr.net/npm/wikiparser-node@browser/extensions/ui.min.css'
-document.head.append(link)
-
 export const CodeJar = (textarea: HTMLTextAreaElement, include?: boolean): CodeJarAsync => {
   const id = wikiparse.id++
   const highlight = async (ele: HTMLElement) => {
@@ -18,7 +13,7 @@ export const CodeJar = (textarea: HTMLTextAreaElement, include?: boolean): CodeJ
   }
 
   const editor = document.createElement('div')
-  const {font, lineHeight, padding, background} = getComputedStyle(textarea)
+  const {font, lineHeight, padding} = getComputedStyle(textarea)
   const {offsetHeight, style: {height}} = textarea
   editor.textContent = textarea.value
   editor.style.height = offsetHeight ? `${offsetHeight}px` : height
@@ -27,11 +22,9 @@ export const CodeJar = (textarea: HTMLTextAreaElement, include?: boolean): CodeJ
   editor.style.font = font
   editor.style.lineHeight = lineHeight
   editor.style.padding = padding
-  editor.style.background = background
   textarea.after(editor)
   textarea.style.display = 'none'
 
-  editor.focus()
   const jar = CodeJarAsync(editor, highlight, {spellcheck: true})
   textarea.form?.addEventListener('submit', () => {
     textarea.value = jar.toString()
