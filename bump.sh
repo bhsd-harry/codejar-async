2e3899735c073c1b361fa48ef440b8b543fae49e
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-12-02 15:18:19 +0800

    chore(bump.sh): update

--- bump.sh
+++ bump.sh
@@ -3,7 +3,11 @@ npm run lint && npm run build
 if [[ $? -eq 0 ]]
 then
 	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+	git fetch
 	git add -A
 	git commit -m "chore: bump version to $1"
 	git push
+	git tag $1
+	git push origin $1
+	npm publish --tag ${2-latest}
 fi
