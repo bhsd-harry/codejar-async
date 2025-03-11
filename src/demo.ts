3341aa4cf3a6e8a646d8361f84a1c5616f378af1
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-11 22:46:25 +0800

    fix(demo.html): prevent favicon request

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,5 @@
 import {CodeJar} from './codejar.js'
-import type {Config} from 'wikiparser-node'
+import type {ConfigData} from 'wikiparser-node'
 import type {} from 'wikiparser-node/extensions/typings.d.ts'
 
 // Must be global before importing `wikiparse.codejar`
@@ -8,7 +8,7 @@ Object.assign(globalThis, {CodeJar});
 (async () => {
   await import('/wikiparser-node/extensions/dist/codejar.js') // eslint-disable-line es-x/no-dynamic-import
 
-  const config: Config = await (await fetch('/wikiparser-node/config/default.json')).json()
+  const config: ConfigData = await (await fetch('/wikiparser-node/config/default.json')).json()
   wikiparse.setConfig(config)
 
   const jar = (await wikiparse.codejar)!(document.querySelector('textarea')!, false, true)
